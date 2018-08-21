a58c6a8af7e519a532bc86d5f0a6574415330427
Aaron Chavez <achavez@corporaciondpt.com>
2018-08-21 12:14:02 -0600

    finished class 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = 'Henry';
+
+  array = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  Percent = 0.234;
+
+  salary = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion:{
+      calle: "First",
+      casa: "19"
+    }
+  } 
 }
